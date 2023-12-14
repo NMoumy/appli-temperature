fc158ebcdf36d649c099045d6da7c7d1c2140813
Moumy Ndiaye <e2177120@cmaisonneuve.qc.ca>
2023-12-13 21:46:03 -0500

    Ajout du graphique

--- src/composants/TemperatureCourbe.jsx
+++ src/composants/TemperatureCourbe.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
 import './TemperatureCourbe.scss';
+import Chart from 'chart.js/auto';
+import React, { useEffect, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+
 
 export default function TemperatureCourbe() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const time = now.getHours() + ':' + now.getMinutes();
+      const temperature = Math.random() * 10 + 20; // Remplacez par la température réelle
+      setData(prevData => {
+        let newData = [...prevData, { time, temperature }];
+        if (newData.length > 10) {
+          // Supprimer la première valeur si la longueur est déjà de 10
+          newData = newData.slice(1);
+        }
+        return newData;
+      });
+    }, 1000); // Mise à jour toutes les 10 minutes
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const chartData = {
+    labels: data.map(d => d.time),
+    datasets: [
+      {
+        label: 'Temperature',
+        data: data.map(d => d.temperature),
+        fill: false,
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgba(255, 99, 132, 0.2)',
+      },
+    ],
+  };
+
   return (
     <div className="bloc temperature-courbe">
-      <h4>temperature</h4>
-      <canvas id="myChart"></canvas>
+      <h4>Temperature</h4>
+      <Line data={chartData} />
     </div>
   );
 }
